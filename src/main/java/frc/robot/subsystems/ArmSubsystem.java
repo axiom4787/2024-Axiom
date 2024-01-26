657fd54777e7cd0dd5f7b05c1f3cf65d8f4b2cbd
Tanmay <mail2tanmaygarg@gmail.com>
2024-01-26 16:47:41 -0600

    State machines
    we made an xbox controller with one method which returns dpad.

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -9,51 +9,76 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmIntakeShooter;
+import frc.robot.Constants.SetPointAngles;
+import frc.robot.XBoxController;
 
 public class ArmSubsystem extends SubsystemBase {
-    private final CANSparkMax armLeftMotor; //neo
-    private final CANSparkMax armRightMotor; //neo
+  private final CANSparkMax armLeftMotor; //neo
+  private final CANSparkMax armRightMotor; //neo
 
-    private final RelativeEncoder armLeftEncoder; //encoder for left arm
-    private final RelativeEncoder armRightEncoder; //encoder for right arm
+  private final RelativeEncoder armLeftEncoder; //encoder for left arm
+  private final RelativeEncoder armRightEncoder; //encoder for right arm
 
-    private static int kP;
-    private static int kI;
-    private static int kD;
+  private static int kP;
+  private static int kI;
+  private static int kD;
+  private static double setPoint;
 
-    private PIDController ArmPID = new PIDController(kP, kI, kD);
+  private static XBoxController xboxcontroller = new XBoxController();
+
+
+  private PIDController ArmPID = new PIDController(kP, kI, kD);
     
-    public ArmSubsystem() {
+  public ArmSubsystem() {
 
-      ArmPID.enableContinuousInput(-180, 180);
+    ArmPID.enableContinuousInput(-180, 180);
 
-      armLeftMotor = new CANSparkMax(ArmIntakeShooter.LEFTARM_MOTORID, CANSparkMax.MotorType.kBrushless);
-      armRightMotor = new CANSparkMax(ArmIntakeShooter.RIGHTARM_MOTORID, CANSparkMax.MotorType.kBrushless);
+    armLeftMotor = new CANSparkMax(ArmIntakeShooter.LEFTARM_MOTORID, CANSparkMax.MotorType.kBrushless);
+    armRightMotor = new CANSparkMax(ArmIntakeShooter.RIGHTARM_MOTORID, CANSparkMax.MotorType.kBrushless);
 
-      armLeftMotor.restoreFactoryDefaults();
-      armRightMotor.restoreFactoryDefaults();
+    armLeftMotor.restoreFactoryDefaults();
+    armRightMotor.restoreFactoryDefaults();
 
-      armLeftMotor.setInverted(true);
-      armRightMotor.setInverted(false);
+    armLeftMotor.setInverted(true);
+    armRightMotor.setInverted(false);
 
-      armRightMotor.follow(armLeftMotor);
+    armRightMotor.follow(armLeftMotor);
 
-      armLeftMotor.setSmartCurrentLimit(ArmIntakeShooter.LEFTARM_MOTOR_CURRENT_LIMIT);
-      armRightMotor.setSmartCurrentLimit(ArmIntakeShooter.RIGHTARM_MOTOR_CURRENT_LIMIT);
+    armLeftMotor.setSmartCurrentLimit(ArmIntakeShooter.LEFTARM_MOTOR_CURRENT_LIMIT);
+    armRightMotor.setSmartCurrentLimit(ArmIntakeShooter.RIGHTARM_MOTOR_CURRENT_LIMIT);
 
-      armLeftMotor.setIdleMode(IdleMode.kBrake);
-      armRightMotor.setIdleMode(IdleMode.kBrake);
+    armLeftMotor.setIdleMode(IdleMode.kBrake);
+    armRightMotor.setIdleMode(IdleMode.kBrake);
 
-      armLeftEncoder = armLeftMotor.getEncoder();
-      armRightEncoder = armRightMotor.getEncoder();
-    }
+    armLeftEncoder = armLeftMotor.getEncoder();
+    armRightEncoder = armRightMotor.getEncoder();
+  }
 
-    public void ArmPID(double kSetpoint) {
-      armLeftMotor.set(ArmPID.calculate(armLeftEncoder.getPosition(), kSetpoint) + ArmIntakeShooter.FEED_FOWARD);
-      ArmPID.reset();
+  public void CalculateArmPID(double kSetpoint) {
+    armLeftMotor.set(ArmPID.calculate(armLeftEncoder.getPosition(), kSetpoint) + ArmIntakeShooter.FEED_FOWARD);
+    ArmPID.reset();
   }
 
   public void ArmMove(double Movement){
     armLeftMotor.set(Movement/2.5); //Change this for more motor power
+  }
+
+    
+  @Override
+  public void periodic() {
+  // This method will be called once per scheduler run
+    if (xboxcontroller.getXboxDpad() == 180) {
+      CalculateArmPID(SetPointAngles.INTAKE_GROUND_ANGLE);
+    }
+    else if (xboxcontroller.getXboxDpad() == 270) {
+      CalculateArmPID(SetPointAngles.SHOOTER_AMP_ANGLE);
+    }
+    else if (xboxcontroller.getXboxDpad() == 90) {
+      CalculateArmPID(SetPointAngles.SHOOTER_SPEAKER_ANGLE);
+    }
+    else if (xboxcontroller.getXboxDpad() == 0) {
+      CalculateArmPID(SetPointAngles.INTAKE_HUMAN_ANGLE);
+    }
+    // need an else statement once triggers ready.
   }
 }
