5f994ed6e6e17bb69363f2bb2f95cf23909e71e5
izzy4787 <153871634+izzy4787@users.noreply.github.com>
2024-02-07 15:58:17 -0600

    Update Climbing.java

--- src/main/java/frc/robot/subsystems/Climbing.java
+++ src/main/java/frc/robot/subsystems/Climbing.java
@@ -1,9 +1,35 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX; 
+import com.ctre.phoenix6.hardware.WPI_VictorSPX; 
 
 public class Climbing extends SubsystemBase{
+
+    private WPI_VictorSPX leftClimberOne, leftClimberTwo, rightClimberOne, rightClimberTwo;
+    private DifferentialDrive differentialDrive;
+
+    // falcon
     public Climbing() {
-        
+        leftClimberOne = new WPI_VictorSPX(Constants.LEFT_CLIMBER_ONE_ID);
+        leftClimberTwo = new WPI_VictorSPX(Constants.LEFT_CLIMBER_TWO_ID);
+        rightClimberOne = new WPI_VictorSPX(Constants.RIGHT_CLIMBER_ONE_ID);
+        rightClimberTwo = new WPI_VictorSPX(Constants.RIGHT_CLIMBER_TWO_ID);
+
+        leftClimberOne.setInverted(Constants.LEFT_CLIMBER_INVERTED);
+        rightClimberOne.setInverted(Constants.RIGHT_CLIMBER_INVERTED);
+
+        // leftClimberOne.follow(rightClimberOne);
+        // leftClimberTwo.follow(rightClimberTwo);
+
+        // differentialDrive = new DifferentialDrive(leftClimberOne, leftClimberTwo);
+    }
+
+    public void climb(double up, double down) {
+        leftClimberOne.set(up, down);
+        leftClimberTwo.set(up, down);
+        rightClimberOne.set(up, down);
+        rightClimberTwo.set(up, down);
+        // differentialDrive.set(up, down);
     }
 }
