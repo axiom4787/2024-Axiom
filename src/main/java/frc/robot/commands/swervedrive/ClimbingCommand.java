a98f04e8df2c9dafb7cda6af56ea02e61f35450d
izzy4787 <153871634+izzy4787@users.noreply.github.com>
2024-02-14 12:07:07 -0600

    climbing motors changed to sparkmax

---
+++ src/main/java/frc/robot/commands/swervedrive/ClimbingCommand.java
@@ -0,0 +1,5 @@
+package main.java.frc.robot.commands.swervedrive;
+
+public class ClimbingCommand {
+    
+}
--- src/main/java/frc/robot/subsystems/Climbing.java
+++ src/main/java/frc/robot/subsystems/Climbing.java
@@ -1,35 +1,39 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX; 
 import com.ctre.phoenix6.hardware.WPI_VictorSPX; 
 
 public class Climbing extends SubsystemBase{
 
-    private WPI_VictorSPX leftClimberOne, leftClimberTwo, rightClimberOne, rightClimberTwo;
-    private DifferentialDrive differentialDrive;
+    private CANSparkMax leftClimber, rightClimber;
+    // private DifferentialDrive differentialDrive;
+    private final RelativeEncoder leftClimberEncoder, rightClimberEncoder;
 
     // falcon
     public Climbing() {
-        leftClimberOne = new WPI_VictorSPX(Constants.LEFT_CLIMBER_ONE_ID);
-        leftClimberTwo = new WPI_VictorSPX(Constants.LEFT_CLIMBER_TWO_ID);
-        rightClimberOne = new WPI_VictorSPX(Constants.RIGHT_CLIMBER_ONE_ID);
-        rightClimberTwo = new WPI_VictorSPX(Constants.RIGHT_CLIMBER_TWO_ID);
+        leftClimber = new CANSparkMax(Constants.LEFT_CLIMBER_ID);
+        rightClimber = new CANSparkMax(Constants.RIGHT_CLIMBER_ID);
 
-        leftClimberOne.setInverted(Constants.LEFT_CLIMBER_INVERTED);
-        rightClimberOne.setInverted(Constants.RIGHT_CLIMBER_INVERTED);
+        leftClimber.setInverted(Constants.LEFT_CLIMBER_INVERTED);
+        rightClimber.setInverted(Constants.RIGHT_CLIMBER_INVERTED);
 
-        // leftClimberOne.follow(rightClimberOne);
-        // leftClimberTwo.follow(rightClimberTwo);
+        // leftClimber.follow(rightClimber);
 
-        // differentialDrive = new DifferentialDrive(leftClimberOne, leftClimberTwo);
+        leftClimber.getSmartCurrentLimit(Constants.LEFT_CLIMBER_CURRENT_LIMIT);
+        rightClimber.getSmartCurrentLimit(Constants.RIGHT_CLIMBER_CURRENT_LIMIT);
+
+        leftClimber.setIdleMode(IdleMode.kBrake);
+        rightClimber.setIdleMode(IdleMode.kBrake);
+
+        leftClimberEncoder = leftClimber.getEncoder();
+        rightClimberEncoder = rightClimber.getEncoder();
     }
 
-    public void climb(double up, double down) {
-        leftClimberOne.set(up, down);
-        leftClimberTwo.set(up, down);
-        rightClimberOne.set(up, down);
-        rightClimberTwo.set(up, down);
-        // differentialDrive.set(up, down);
+    public void climb(double speed) {
+        rightClimber.set(speed);
+        leftClimber.set(speed);
     }
 }
